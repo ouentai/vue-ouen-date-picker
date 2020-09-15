d99890ad6b656fa6a186a71f7595cbbbfe040240
rky <rky@frontier>
2020-09-15 11:48:33 +0900

    comment

--- mixin/Calendarable.js
+++ mixin/Calendarable.js
@@ -21,20 +21,20 @@
     - see above example, you can customize props and @ event.
 
   interface : BaseCalendar
-  - input : props
-    - border : boolean : カレンダーの枠線の有無
-    - ym : number : YYYYMM型 190001-209912: year monthより優先 : 月が範囲を越えると自動で年で調整する Dateの扱い
-    - year : number : YYYY型
-    - month : number : 1-12
-    - week-labels : array <labels> : array.length must be 7 : 曜日の色とか
-      - labels : object : { value : string , color : string }
-    - start-week : number : 0-6 : 0:sun, 1:mon ...
-    - disabled : boolean : clickイベントを無効にする
-    - checked-color : string : cssで使える値 : clickedで渡されたものの中にあったときの背景色
-    - checked : array <number> : 背景を変えたい日付の配列
-  - output : emit
-    - info:calendar : object : 表示されているカレンダーの情報
-    - click:day : number : クリックされた日付
+    - input : props
+      - border : boolean : カレンダーの枠線の有無
+      - ym : number : YYYYMM型 190001-209912: year monthより優先 : 月が範囲を越えると自動で年で調整する Dateの扱い
+      - year : number : YYYY型
+      - month : number : 1-12
+      - week-labels : array <labels> : array.length must be 7 : 曜日の色とか
+        - labels : object : { value : string , color : string }
+      - start-week : number : 0-6 : 0:sun, 1:mon ...
+      - disabled : boolean : clickイベントを無効にする
+      - checked-color : string : cssで使える値 : clickedで渡されたものの中にあったときの背景色
+      - checked : array <number> : 背景を変えたい日付の配列
+    - output : emit
+      - info:calendar : object : 表示されているカレンダーの情報
+      - click:day : number : クリックされた日付
 */
 
 import BaseCalendar from '../components/BaseCalendar';
